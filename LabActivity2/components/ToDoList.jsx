51de8f38594823aab7b9b4924591eca4875a7485
yyuuaan <98503779+yyuuaan@users.noreply.github.com>
2024-01-16 14:33:06 +0800

    Add files via upload

---
+++ LabActivity2/components/ToDoList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default class ToDoList extends React.Component {
+    render() { 
+        return(
+            <div>
+                <h1>To do list</h1>
+                <ul>
+                    <li>Clean Porch</li>
+                    <li>Wash Car</li>
+                    <li>Attend BCHAIN Class</li>
+                </ul>
+            </div>
+    )
+    }
+}
+
+// import React from "react";
+
+// export default function ToDoList() {
+//     return(
+//         <div>
+//             <h1>To do list</h1>
+//             <ul>
+//                 <li>Clean Porch</li>
+//                 <li>Wash Car</li>
+//                 <li>Attend BCHAIN Class</li>
+//             </ul>
+//         </div>
+//     );
+// }
